5cefcbeeb768b3c5298c61aa46ec0bd9c41ac153
annauzun <terziann@mail.ru>
2023-07-05 06:06:51 -0500

    js corrected

--- script.js
+++ script.js
@@ -11,21 +11,19 @@ if(username) {
 
 let count = 0
 
-
-
 function addTask () {
     count ++
-    let myContainer = document.getElementById("container")
-    let myInput = document.getElementById("input")
-    
-    let value = myInput.value
-   
+    const myContainer = document.getElementById("container")
+    const myInput = document.getElementById("input")
+    const value = myInput.value
+
     if (count <= 5) {
         myContainer.innerHTML = myContainer.innerHTML + "<li>" + value + `<input type="checkbox" class="ckeck-box">` + `<button id="delete-task">Удалить</button>` + "</li>" 
     } else {
         alert("Вы ввели больше 5 дел!")
     }   
 }
+
 
 const addTaskButton = document.getElementById('button')
 addTaskButton.addEventListener('click', addTask)
